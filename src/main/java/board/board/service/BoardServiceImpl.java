c1d75132aee01316262e5cfdc2c23b7dd65fe3cd
Hyungjun Ahn <kingofahn@Hyungjuns-MacBook-Pro.local>
2020-07-10 18:07:45 +0900

    first commit

---
+++ src/main/java/board/board/service/BoardServiceImpl.java
@@ -0,0 +1,43 @@
+package board.board.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import board.board.dto.BoardDto;
+import board.board.mapper.BoardMapper;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+	
+	@Autowired
+	private BoardMapper boardMapper;
+	
+	@Override
+	public List<BoardDto> selectBoardList() throws Exception {
+		return boardMapper.selectBoardList();
+	}
+
+	@Override
+	public void insertBoard(BoardDto board) throws Exception {
+		boardMapper.insertBoard(board);
+	}
+
+	@Override
+	public BoardDto selectBoardDetail(int boardIdx) throws Exception {
+		boardMapper.updateHitCount(boardIdx);
+		return boardMapper.selectBoardDetail(boardIdx);
+	}
+
+	@Override
+	public void updateBoard(BoardDto boardDto) throws Exception{
+		boardMapper.updateBoard(boardDto);
+	}
+
+	@Override
+	public void deleteBoard(int boardIdx) throws Exception{
+		boardMapper.deleteBoard(boardIdx);
+	}
+}	
+
